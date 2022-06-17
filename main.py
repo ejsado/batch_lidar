c6187ed8cb9a3486c201424c666357730f34c5e5
ejsado <ejsado@users.noreply.github.com>
2022-06-16 20:42:24 -0400

    convert las

--- main.py
+++ main.py
@@ -7,5 +7,16 @@ from options import *
 
 if __name__ == '__main__':
 	print("Running batch_lidar")
-
+	if convertToLAS:
+		print("Converting files in " + convertFolder)
+		result = arcpy.conversion.ConvertLas(
+			convertFolder,
+			outputFolder,
+			las_options=[],
+			define_coordinate_system="ALL_FILES",
+			in_coordinate_system=definedCRS
+		)
+		if verboseOutput: print(result)
+	if createLasDataset:
+		print("Creating LAS Dataset")
 
--- options.py
+++ options.py
@@ -16,11 +16,24 @@ verboseOutput = True
 
 # select which operations to perform
 # set these values to false if you wish to skip certain steps
+
+# convert a directory of lidar data files to .las
 convertToLAS = True
+# create a dataset containing a collection of .las files
+createLasDataset = True
 
 # LAS/LAZ/ZLAS directory
 # all matching files in the directory will be converted
 convertFolder = r"D:\Projects_Tower2019\GIS\Lidar\data\LAZ"
+
+# coordinate system for the input LAS/LAZ/ZLAS files
+# https://pro.arcgis.com/en/pro-app/latest/arcpy/classes/spatialreference.htm
+# all files in the above directory must be in the same CRS defined here
+# (horizontal CRS, vertical CRS)
+definedCRS = arcpy.SpatialReference(6342, 5703)
+
+# output folder for files converted to LAS
+outputFolder = r"D:\Projects_Tower2019\GIS\Lidar\data\LAS_output"
 
 # text to append to converted LAS files
 convertedText = "_converted"
